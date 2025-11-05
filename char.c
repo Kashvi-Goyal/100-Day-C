575ad536e682661847daf979f4361d35d04444e7
Kashvi_Goyal <kashvi.goyal.0310@gmail.com>
2025-11-05 09:46:22 +0530

    Add files via upload

--- char.c
+++ char.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
 int main()
-{
+ {
     char ch;
-    printf("Enter a character");
+    printf("Enter a character=");
     scanf(" %c", &ch);
 
-    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') 
+    if (ch >= 'A' && ch <= 'Z') 
     {
-    printf("Vowel\n");
-    } 
+        printf("Uppercase alphabet\n");
+    }
+    else if (ch >= 'a' && ch <= 'z')
+    {
+        printf("Lowercase alphabet\n");
+    }
+    else if (ch >= '0' && ch <= '9') 
+    {
+        printf("Digit\n");
+    }
     else 
     {
-    printf("Consonant\n");
+        printf("Special character\n");
     }
-
 }
