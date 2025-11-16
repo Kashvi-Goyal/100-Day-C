2b0d80cfcfcf6b06bd8eae5f50d06e229fa49e99
Kashvi_Goyal <kashvi.goyal.0310@gmail.com>
2025-11-16 19:17:27 +0530

    Add files via upload

---
+++ sum_of_digits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, sum = 0, remainder;
+
+    scanf("%d", &n);
+
+    while (n != 0) 
+    {
+        remainder = n % 10;
+        
+        sum += remainder;
+        n /= 10;
+    }
+    printf("%d\n", sum);
+}
